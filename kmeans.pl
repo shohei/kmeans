494590921a9cb53877c7cbd3045a36395a17828e
Shohei Aoki <shoaok@gmail.com>
2016-11-12 16:15:48 +0900

    make prototype

--- kmeans.pl
+++ kmeans.pl
@@ -3,45 +3,160 @@ use warnings;
 
 use Moo;
 use feature qw(say);
+use Switch;
 use Data::Dumper;
+use List::Util qw(first max maxstr min minstr reduce shuffle sum);
 
-# use GD::Graph::points;
+# k-means clustering
+# k=3
+# iris data from R
 
-# my $graph = GD::Graph::points->new;
-# $graph->set( 
-#         x_label     => 'Label', 
-#         y_label     => 'Values', 
-#         title          => 'Label By Value', 
-#         dclrs         => [ 'black', 'black','black','black','black','black','black'], 
-#         borderclrs     => [ qw(black black), qw(black black) ], 
-#         bar_spacing => 4, 
-#         transparent => 1,
-#         show_values => 1, 
-# ); 
+# initialize constant
+my $loop_count = 0;
+my $smax=0;
+my $smin=1000; #should be large enough
+my $lmax=0;
+my $lmin=1000; #should be large enough
+my $global_max; 
+my $global_min; 
 
-open(IN, "< irisdata.txt");
-my $count = 0;
+my @points;
+sub read_file(){
+    open(IN, "< irisdata.txt");
+	while(<IN>){
+		if($loop_count==0){
+			$loop_count++; 
+			next;
+		}
+		my @array = split('\s',$_);
+    push(@array,0); # this is label (=k)
 
-my @result;
-while(<IN>){
-  if($count==0){
-    $count++; 
-    next;
-  }
-  my @array = split('\s',$_);
-  push(@result, @array);
+    $smax=max($smax,$array[0]);
+    $smin=min($smin,$array[0]);
+    $lmax=max($lmax,$array[1]);
+    $lmin=min($lmin,$array[1]);
+
+    push(@points, \@array);
+}
+close(IN);
+$global_max = max($smax,$lmax);
+$global_min = min($smin,$lmin);
 }
 
-print Dumper @result;
+my @centers; 
 
-# foreach my $array_reference(@result){
-#   foreach my $a(@$array_reference){
-#     print $a;
-#   }
-#   print '--------------';
-# }
+sub select_center{
+	for(my $i=0;$i<3;$i++){
+	  my $xc = rand($global_max-$global_min)+$global_min;
+	  my $yc = rand($global_max-$global_min)+$global_min;
+	  my $pair = [$xc,$yc];
+	  @centers[$i] = $pair;
+	}
+}
 
-close(IN);
+sub assign_label{
+	foreach my $point(@points){
+		my $xp = $point->[0];
+		my $yp = $point->[1];
+		my $label = 0;
+		my $min_norm = $global_max**2; # should be large enough
+		foreach my $center(@centers){
+			my $xc = $center->[0];
+			my $yc = $center->[1];
+			my $norm = sqrt(($xp-$xc)**2 + ($yp-$yc)**2);
+			if($norm<$min_norm){
+				$point->[3] = $label;
+				$min_norm = $norm;
+			}
+			$label++;
+		}
+	}
+}
+
+sub recalculate_center(){
+	my @x0s;
+	my @y0s;
+	my @x1s;
+	my @y1s;
+	my @x2s;
+	my @y2s;
+	foreach my $point(@points){
+		switch($point->[3]){
+			case 0 {
+			  push(@x0s,$point->[0]);
+			  push(@y0s,$point->[1]);
+			}
+			case 1 {
+			  push(@x1s,$point->[0]);
+			  push(@y1s,$point->[1]);
+			}
+			case 2 {
+			  push(@x2s,$point->[0]);
+			  push(@y2s,$point->[1]);
+			}
+		}
+	}
+
+	my $new_x0;
+	my $new_y0;
+	my $count;
+  	$count = $#x0s + 1;
+	if($count>0){
+	  for (my $i=0;$i<$count;$i++){
+		$new_x0 += $x0s[$i];
+		$new_y0 += $y0s[$i];
+	  }
+	  $new_x0 = $new_x0 / $count;
+	  $new_y0 = $new_y0 / $count;
+	  my $new_pair0 = [$new_x0,$new_y0];
+	  @centers[0] = $new_pair0;
+	}
+
+	my $new_x1;
+	my $new_y1;
+	$count = $#x1s + 1;
+	if($count>0){
+	  for (my $i=0;$i<$count;$i++){
+		  $new_x1 += $x1s[$i];
+		  $new_y1 += $y1s[$i];
+	  }
+	  $new_x1 = $new_x1 / $count;
+	  $new_y1 = $new_y1 / $count;
+	  my $new_pair1 = [$new_x1,$new_y1];
+	  @centers[1] = $new_pair1;
+	} 
+
+	my $new_x2;
+	my $new_y2;
+	$count = $#x2s + 1;
+	if($count>0){
+	  for (my $i=0;$i<$count;$i++){
+		  $new_x2 += $x2s[$i];
+		  $new_y2 += $y2s[$i];
+	  }
+	  $new_x2 = $new_x2 / $count;
+	  $new_y2 = $new_y2 / $count;
+	  my $new_pair2 = [$new_x2,$new_y2];
+	  @centers[2] = $new_pair2;
+	} 
+}
+
+sub k_means(){
+	read_file();
+
+	for(my $i=0;$i<1000;$i++){
+  	  select_center();
+	  assign_label();
+	  recalculate_center();
+	  say "loop $i has finished";
+	}
+
+	say 'computation done.';
+	say Dumper @centers;
+}
+
+k_means();
+
 
 
 
